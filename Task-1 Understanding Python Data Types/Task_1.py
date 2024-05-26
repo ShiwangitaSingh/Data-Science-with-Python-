09f6bb275e3be510f2b7b1af9bc6ec5db908f5ae
Shiwangita Singh <shiwangitasingh@gmail.com>
2024-05-26 19:43:23 +0530

    Task 1
    Completed Successfully

---
+++ Task-1 Understanding Python Data Types/Task_1.py
@@ -0,0 +1,56 @@
+# Creating a list
+my_list = [1, 2, 3]
+
+# Basic operations on a list
+print("Original list:", my_list)
+
+# Adding an element to the list
+my_list.append(4)
+print("List after adding an element:", my_list)
+
+# Removing an element from the list
+my_list.remove(2)
+print("List after removing an element:", my_list)
+
+# Modifying an element in the list
+my_list[1] = 5
+print("List after modifying an element:", my_list)
+
+
+
+# Creating a dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Basic operations on a dictionary
+print("\nOriginal dictionary:", my_dict)
+
+# Adding an element to the dictionary
+my_dict['d'] = 4
+print("Dictionary after adding an element:", my_dict)
+
+# Removing an element from the dictionary
+del my_dict['b']
+print("Dictionary after removing an element:", my_dict)
+
+# Modifying an element in the dictionary
+my_dict['a'] = 10
+print("Dictionary after modifying an element:", my_dict)
+
+
+
+# Creating a set
+my_set = {1, 2, 3}
+
+# Basic operations on a set
+print("\nOriginal set:", my_set)
+
+# Adding an element to the set
+my_set.add(4)
+print("Set after adding an element:", my_set)
+
+# Removing an element from the set
+my_set.remove(2)
+print("Set after removing an element:", my_set)
+
+# Note: Sets do not support indexing or modifying specific elements directly
+# because they are unordered collections. However, you can add and remove elements.
